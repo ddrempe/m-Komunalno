01938d59375cc408b379a3acb6559d23e5c57aed
Ivan Šincek <ivasincek@foi.hr>
2017-11-03 19:17:13 +0100

    Login interface (#1)
    * Login Interface
    
    Required installations:
    npm install
    npm install react-navigation --save
    npm install nachos-ui --save
    npm install react-native-vector-icons --save
    react-native link react-native-vector-icons
    
    * Login Interface
    
    Inline styles are now in main style sheet. Some tags require style to be referenced by object (i.e. style ID).
    
    * Comment Fixed
    
    Added "//TODO" to an empty method.

---
+++ app/scenes/loginScene.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+import {
+  Input,
+  Button
+} from 'nachos-ui';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class LoginScene extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  onSubmit() {
+    //TODO
+  }
+
+  render() {
+    return (
+      <View style={stylesContainer}>
+        <View style={stylesRow}>
+          <View style={stylesIcon}>
+            <Icon
+              size={20}
+              name='user'
+            />
+          </View>
+          <Input
+            style={stylesInput}
+            height={52}
+            underlineColorAndroid='transparent'
+            autoCapitalize='none'
+            placeholder='Korisničko ime'
+            value={this.state.username}
+            onChangeText={username => this.setState({username})}
+          />
+        </View>
+        <View style={stylesRow}>
+            <View style={stylesIcon}>
+              <Icon
+                size={20}
+                name='key'
+              />
+            </View>
+          <Input
+            style={stylesInput}
+            height={52}
+            underlineColorAndroid='transparent'
+            secureTextEntry={true}
+            placeholder='Lozinka'
+            value={this.state.password}
+            onChangeText={password => this.setState({password})}
+          />
+        </View>
+        <View>
+          <Button
+            style={stylesButton}
+            kind='squared'
+            iconSize={20}
+            iconPosition='left'
+            iconName='md-log-in'
+            uppercase={false}
+            children='Prijavi se'
+            onPress = {this.onSubmit}
+          >
+          </Button>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E4E4E4',
+    justifyContent: 'center',
+    flex: 1,
+    paddingRight: 12,
+    paddingLeft: 12
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16
+  },
+  icon: {
+    width: 24,
+  },
+  input: {
+    flex: 1
+  },
+  button: {
+    backgroundColor: '#70B5E5',
+    height: 52
+  }
+});
+
+var stylesContainer = StyleSheet.flatten([styles.container]);
+var stylesRow = StyleSheet.flatten([styles.row]);
+var stylesIcon = StyleSheet.flatten([styles.icon]);
+var stylesInput = StyleSheet.flatten([styles.input]);
+var stylesButton = StyleSheet.flatten([styles.button]);
