ede3b6436581f5ce84668f03738507c8244f8a51
Ivan Šincek <ivasincek@foi.hr>
2017-11-29 22:07:10 +0100

    Grid View With List VIew Option
    Removed react-native-super-grid and made grid view with native RN element "FlatList" and added own CSS styling. Added new button to toggle view between grid and list.
    
    Reason for removing react-native-super-grid is that there is no easy way to redesign grid in list like view.

--- app/scenes/mainScene.js
+++ app/scenes/mainScene.js
@@ -1,75 +1,124 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  TouchableHighlight,
   View,
+  FlatList,
+  TouchableHighlight,
   Image,
   Text,
+  Button,
+  Dimensions,
   Alert
 } from 'react-native';
-import GridView from 'react-native-super-grid';
 
 export default class MainScene extends Component<{}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'Grid View',
+      columns: 2,
+      key: 1
+    };
+  }
+
+  onToggle() {
+    if (this.state.columns != 2) {
+      this.setState({view: 'Grid View', columns: 2, key: 1})
+    }
+    else {
+      this.setState({view: 'List View', columns: 1, key: 2})
+    };
+  }
+
   onTileClick() {
     Alert.alert('Tile Click');
-    //TODO
+    //TODO: change scene on click
   }
 
   render() {
-    //hardcoded temporary tiles for design purposes
-    const tiles = [
-      { id: 1, title: 'Title 1', subtitle: 'Subtitle 1'},
-      { id: 2, title: 'Title 2', subtitle: 'Subtitle 2'},
-      { id: 3, title: 'Title 3', subtitle: 'Subtitle 3'},
-      { id: 4, title: 'Title 4', subtitle: 'Subtitle 4'},
-      { id: 5, title: 'Title 5', subtitle: 'Subtitle 5'},
-      { id: 6, title: 'Title 6', subtitle: 'Subtitle 6'},
-      { id: 7, title: 'Title 7', subtitle: 'Subtitle 7'}
+    //TODO: tiles data needs to be pulled from web API
+    var tiles = [
+      { id: 1, title: 'Title 1', description: 'Description 1', image: require('../images/default-settings.png') },
+      { id: 2, title: 'Title 2', description: 'Description 2', image: require('../images/default-settings.png') },
+      { id: 3, title: 'Title 3', description: 'Description 3', image: require('../images/default-settings.png') },
+      { id: 4, title: 'Title 4', description: 'Description 4', image: require('../images/default-settings.png') },
+      { id: 5, title: 'Title 5', description: 'Description 5', image: require('../images/default-settings.png') },
+      { id: 6, title: 'Title 6', description: 'Description 6', image: require('../images/default-settings.png') },
+      { id: 7, title: 'Title 7', description: 'Description 7', image: require('../images/default-settings.png') }
     ];
 
     return (
-      <GridView
-        style={stylesGridView}
-        spacing={4}
-        itemWidth={170}
-        items={tiles}
-        renderItem={(tile) => (
-          <TouchableHighlight
-            underlayColor='black'
-            onPress={this.onTileClick}
-          >
-            <View style={stylesTile}>
-              <View style={{alignItems: 'center'}}>
-                <Image
-                  style={stylesImage}
-                  source={require('../images/default-settings.png')}
-                />
+      <View style={stylesContainer}>
+        <Button
+          color='#70B5E5'
+          title={this.state.view}
+          onPress = {this.onToggle.bind(this)}
+        >
+        </Button>
+        <FlatList
+          contentContainerStyle={stylesFlatList}
+          key={this.state.key}
+          numColumns={this.state.columns}
+          data={tiles}
+          renderItem={({item}) => (
+            <TouchableHighlight
+              underlayColor='black'
+              onPress={this.onTileClick}
+            >
+              <View style={this.state.key != 2 ? stylesTileGrid : stylesTileList}>
+                <View style={{alignItems: 'center'}}>
+                  <Image
+                    style={stylesImage}
+                    source={item.image}
+                  />
+                </View>
+                <View style={this.state.key != 2 ? stylesTextGrid : stylesTextList}>
+                  <Text style={stylesTitle}>{item.title}</Text>
+                  <Text style={stylesSubtitle}>{item.description}</Text>
+                </View>
               </View>
-              <Text style={stylesTitle}>{tile.title}</Text>
-              <Text style={stylesSubtitle}>{tile.subtitle}</Text>
-            </View>
-          </TouchableHighlight>
-        )}
-      />
+            </TouchableHighlight>
+          )}
+          keyExtractor={(item, index) => (item.id)}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  gridView: {
+  container: {
     backgroundColor: '#E4E4E4',
     flex: 1
   },
-  tile: {
+  flatList: {
+    margin: 2
+  },
+  tileGrid: {
     backgroundColor: '#FFFFFF',
+    margin: 2,
+    padding: 10,
+    width: Dimensions.get('window').width / 2 - 6
+  },
+  tileList: {
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 2,
     padding: 10
+    
   },
   image: {
     height: 72,
     width: 72
   },
+  textGrid: {
+    paddingTop: 10
+  },
+  textList: {
+    paddingLeft: 10
+  },
   title: {
-    paddingTop: 10,
     color: '#000000',
     fontSize: 16,
     fontWeight: 'bold'
@@ -79,8 +128,12 @@ const styles = StyleSheet.create({
   }
 });
 
-var stylesGridView = StyleSheet.flatten([styles.gridView]);
-var stylesTile = StyleSheet.flatten([styles.tile]);
+var stylesContainer = StyleSheet.flatten([styles.container]);
+var stylesFlatList = StyleSheet.flatten([styles.flatList]);
+var stylesTileGrid = StyleSheet.flatten([styles.tileGrid]);
+var stylesTileList = StyleSheet.flatten([styles.tileList]);
 var stylesImage = StyleSheet.flatten([styles.image]);
+var stylesTextGrid = StyleSheet.flatten([styles.textGrid]);
+var stylesTextList = StyleSheet.flatten([styles.textList]);
 var stylesTitle = StyleSheet.flatten([styles.title]);
 var stylesSubtitle = StyleSheet.flatten([styles.subtitle]);
